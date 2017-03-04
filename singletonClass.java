99cd501fb80fd123c9d56fc1771efdc1bd1e6808
alexma3109 <myx@Yixins-MacBook-Pro.local>
2017-03-03 23:41:33 -0800

    fix

--- singletonClass.java
+++ singletonClass.java
@@ -1,10 +1,18 @@
-Singleton Class
-
+//Singleton Class//
+public class Singleton{
+	private static SingletonHolder{
+		private static final Singleton INSTANCE = new Singleton();
+	}
+	private Singleton(){...}
+	public static final Singleton getInstance(){
+		return SingletonHolder.INSTANCE;
+	}
+}
+///static nested, recommended. ///////////////////
+//////////////////////////////////////
 public class Singleton {
 	private static Singleton instance = null;
-	private Singleton {
-		
-	}
+	private Singleton (){...}
 	public Singleton getInstance(){
 		if(instance == null){
 			instance = new Singleton();
@@ -12,24 +20,20 @@ public class Singleton {
 		return instance;
 	}
 }
-///lazy mode, instantiated when using///
+///lazy mode, instantiated when using. Not thread safe.///
 ////////////////////////////////////////
 public class Singleton {
 	private static final Singleton instance = new Singleton();
-	private Singleton{
-		
-	}
+	private Singleton(){...}
 	public Singleton getInstance(){
 		return instance;
 	}
 }
-///hungry mode, instantiated when loading///
+///hungry mode, instantiated when loading, thread safe.///
 //////////////////////////////////////////// 
 public class Singleton{
 	private static Singleton instance = null;
-	private Singleton {
-		
-	}
+	private Singleton (){...}
 	public synchronized static Singleton getInstance(){
 		if(instance == null){
 			instance = new Singleton();
@@ -37,5 +41,21 @@ public class Singleton{
 		return instance;
 	}
 }
-///synchronized///////////////////
+///synchronized but not efficient. ///////////////////
+//////////////////////////////////////
+public class Singleton{
+	private volatile static Singleton instance = null;
+	private Singleton(){...}
+	public static Singleton getInstance(){
+		if(instance == null){
+			synchronized (Singleton.class){
+				if(instance == null){
+					instance = new Singleton();
+				}
+			}
+		}
+		return instance;
+	}
+}
+///double checked locking pattern, synchronized. ///////////////////
 //////////////////////////////////////
